45717163a915cc4c7d943d813a405ed5603e075b
FrancescoBoco <francesco.bocoux@gmail.com>
2023-09-12 09:08:33 +0200

    Create cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,30 @@ Homepage
     Homepage
 </h1>
 
-<div class="gatto"></div>
 
-<img src="{{ Vite::asset('resources/img/gatto.jpg') }}" class="img-fluid" alt="">
+
+    @foreach ($comics as $comic)
+        <div class="col-3 mb-4">
+            <div class="card">
+                <img src="{{ $comic['thumb'] }}" class="card-img-top" alt="{{ $comic['title'] }}">
+                <div class="card-body">
+                    <h2 class="text-primary">
+                        {{ $comic['title'] }}
+                    </h2>
+                    <h3>
+                        {{ $comic['series'] }}
+                    </h3>
+                    <h4 class="text-success">
+                        {{ $comic['price'] }}
+                    </h4> 
+
+                    <a href="" class="btn btn-primary">
+                        Check single comic
+                    </a>
+                </div>
+            </div>
+        </div>
+        
+    @endforeach
+
 @endsection
