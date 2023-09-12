0cc74db8f2bbfa01a68db9ca819f487bff0593a2
FrancescoBoco <francesco.bocoux@gmail.com>
2023-09-12 10:42:18 +0200

    ADD row

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,29 +12,31 @@ Homepage
 </h1>
 
 
+<div class="row">
 
     @foreach ($comics as $comic)
-        <div class="col-3 mb-4">
-            <div class="card">
-                <img src="{{ $comic['thumb'] }}" class="card-img-top" alt="{{ $comic['title'] }}">
-                <div class="card-body">
-                    <h2 class="text-primary">
-                        {{ $comic['title'] }}
-                    </h2>
-                    <h3>
-                        {{ $comic['series'] }}
-                    </h3>
-                    <h4 class="text-success">
-                        {{ $comic['price'] }}
-                    </h4> 
-
-                    <a href="" class="btn btn-primary">
-                        Check single comic
-                    </a>
-                </div>
+    <div class="col-3 mb-4">
+        <div class="card">
+            <img src="{{ $comic['thumb'] }}" class="card-img-top" alt="{{ $comic['title'] }}">
+            <div class="card-body">
+                <h2 class="text-primary">
+                    {{ $comic['title'] }}
+                </h2>
+                <h3>
+                    {{ $comic['series'] }}
+                </h3>
+                <h4 class="text-success">
+                    {{ $comic['price'] }}
+                </h4> 
+                
+                <a href="" class="btn btn-primary">
+                    Check single comic
+                </a>
             </div>
         </div>
-        
+    </div>
+    
     @endforeach
+</div>
 
 @endsection
